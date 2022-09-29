39fa99fddab26c9445de47206b84630f8a0c72fa
十块钱 <roy2651@gmail.com>
2022-09-29 15:10:57 +0800

    1

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "十块钱",
+  description: "十块钱的博客",
+  lastUpdated: true,
+  themeConfig: {
+    nav: [
+        { text: "首页", link: "/" }, 
+        {
+            text: "飞书",
+            link: "/started",
+          },
+    ]
+  }
+};
