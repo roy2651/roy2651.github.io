81113f2abc6a4b60818bc011c052dfd83e2fa3e2
十块钱 <roy2651@gmail.com>
2025-03-27 12:17:22 +0800

    提交

---
+++ assets/notes_feishu.md.DapuVl_b.js
@@ -0,0 +1,31 @@
+import{_ as n,c as a,o as t,af as r}from"./chunks/framework.0BqVfsWd.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"notes/feishu.md","filePath":"notes/feishu.md","lastUpdated":1666858846000}'),p={name:"notes/feishu.md"};function s(o,e,h,c,i,l){return t(),a("div",null,e[0]||(e[0]=[r(`<h2 id="创建企业应用-https-open-feishu-cn-app-lang-zh-cn" tabindex="-1">创建企业应用（<a href="https://open.feishu.cn/app/?lang=zh-CN%EF%BC%89" target="_blank" rel="noreferrer">https://open.feishu.cn/app/?lang=zh-CN）</a> <a class="header-anchor" href="#创建企业应用-https-open-feishu-cn-app-lang-zh-cn" aria-label="Permalink to &quot;创建企业应用（https://open.feishu.cn/app/?lang=zh-CN）&quot;">​</a></h2><pre><code>1. 进入飞书开放平台: 企业自建应用-企业自建应用
+2. 输入: 应用名称、应用描述、上传图标
+</code></pre><h2 id="权限配置" tabindex="-1">权限配置 <a class="header-anchor" href="#权限配置" aria-label="Permalink to &quot;权限配置&quot;">​</a></h2><pre><code>1. 查看AppID,AppSecret
+2. 启用机器人
+3. 配置IP白名单核回调域
+4. 配置应用接口权限
+    用户 
+        获取用户邮箱信息
+        获取用户手机号
+        获取用户 user ID
+        通过手机号或邮箱获取用户 ID
+        获取用户基本信息
+        更新应用创建群聊的信息
+    消息
+        以应用的身份发消息（历史版本）
+        获取用户发给机器人的单聊消息（历史版本）
+        获取用户在群聊中@机器人的消息（历史版本）
+    通讯录
+        获取用户雇佣信息
+        获取用户性别
+        获取用户组织架构信息
+        更新用户组信息
+        获取用户组信息
+        获取角色权限
+        获取部门组织架构信息
+        获取部门基础信息
+        更新通讯录
+        以应用身份访问通讯录
+        
+5. 飞书应用权限变更是需要发布版本才会生效
+</code></pre>`,4)]))}const _=n(p,[["render",s]]);export{f as __pageData,_ as default};
